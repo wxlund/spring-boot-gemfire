984ec9a6d7c5f53f227eaa96c1f8b86c0551a967
Zac Hansen <zhansen@pivotal.io>
2019-01-10 16:12:42 -0700

    added working dependencies and have running build :: need to upgrade to current gemfire version

--- src/main/java/io/pivotal/gemfire/server/SpringBootGemfireApplication.java
+++ src/main/java/io/pivotal/gemfire/server/SpringBootGemfireApplication.java
@@ -1,21 +1,44 @@
+/*
+ * Copyright  2017
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 package io.pivotal.gemfire.server;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.gemfire.config.annotation.CacheServerApplication;
 import org.springframework.data.gemfire.config.annotation.EnableLocator;
 import org.springframework.data.gemfire.config.annotation.EnableManager;
 import org.springframework.session.data.gemfire.config.annotation.web.http.EnableGemFireHttpSession;
+import org.springframework.session.data.gemfire.config.annotation.web.http.GemFireHttpSessionConfiguration;
 
 @CacheServerApplication
+@EnableGemFireHttpSession(
+        regionName = "Sessions",
+        sessionSerializerBeanName = GemFireHttpSessionConfiguration.SESSION_DATA_SERIALIZER_BEAN_NAME
+)
+@EnableLocator
 @EnableManager(start = true)
-@EnableLocator(port = 10334)
-@EnableGemFireHttpSession(regionName = "Session")
-@SpringBootApplication
 public class SpringBootGemfireApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringBootGemfireApplication.class, args);
-  }
 
+    new SpringApplicationBuilder(SpringBootGemfireApplication.class)
+            .web(WebApplicationType.NONE)
+            .build()
+            .run(args);
+  }
 }
