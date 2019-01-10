a9388c8beb06b7bc3a647fea3157018872e6a455
Wayne Lund <wlund@wlundsmcpro2018.nv.charter.com>
2019-01-10 14:23:11 -0800

    first commit

---
+++ src/main/java/io/pivotal/gemfire/server/SpringBootGemfireApplication.java
@@ -0,0 +1,14 @@
+package io.pivotal.gemfire.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGemfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGemfireApplication.class, args);
+	}
+
+}
+
