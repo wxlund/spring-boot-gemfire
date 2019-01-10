2be4c9c526973a668371240abfa97055133789cc
Wayne Lund <wlund@wlundsmcpro2018.nv.charter.com>
2019-01-10 14:54:10 -0800

    updated pom, properties and main class

--- src/main/java/io/pivotal/gemfire/server/SpringBootGemfireApplication.java
+++ src/main/java/io/pivotal/gemfire/server/SpringBootGemfireApplication.java
@@ -2,13 +2,20 @@ package io.pivotal.gemfire.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.gemfire.config.annotation.CacheServerApplication;
+import org.springframework.data.gemfire.config.annotation.EnableLocator;
+import org.springframework.data.gemfire.config.annotation.EnableManager;
+import org.springframework.session.data.gemfire.config.annotation.web.http.EnableGemFireHttpSession;
 
+@CacheServerApplication
+@EnableManager(start = true)
+@EnableLocator(port = 10334)
+@EnableGemFireHttpSession(regionName = "Session")
 @SpringBootApplication
 public class SpringBootGemfireApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGemfireApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootGemfireApplication.class, args);
+  }
 
 }
-
